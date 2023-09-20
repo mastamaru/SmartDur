2ab8b5e40489b50a085c11a155990452b97b0469
bagusrakaop <brakaputra@gmail.com>
2023-09-20 13:58:54 +0700

    UI: Confirmation Page, Input Page, User Menu Window | Class: Initialize Crop, Initialize Fertilizer

---
+++ SmartDur/Fertilizer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartDur
+{
+    internal class Fertilizer
+    {
+        private string name;
+        private string description;
+        private double nitrogen;
+        private double phospor;
+        private double kalium;
+
+        public string getName()
+        {
+            // Note: Need to get name from database
+            return name;
+        }
+
+        public string getDescription()
+        {
+            // Note: Need to get description from database
+            return description;
+        }
+
+        public (double, double, double) getComposition()
+        {
+            // Note: Need to get fertilizer composition from database
+            return (nitrogen, phospor, kalium); 
+        }
+    }
+}
