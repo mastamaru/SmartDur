d5a857134d66f089dca878b39422816f0a580866
Masta <muhammad.al.ghifari.fausta@mail.ugm.ac.id>
2023-09-21 16:39:05 +0700

    UI: admin menu | Class : Scheduling and Admin

---
+++ SmartDur/Admin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartDur
+{
+    internal class Admin
+    {
+        private String username;
+        private String password;
+
+        public String getUsername()
+        {
+            return username;
+        }
+
+        public String getPassword()
+        {
+            return password;
+        }
+
+        public void Login() { }
+        public void modifyDatabase() { }
+    }
+}
