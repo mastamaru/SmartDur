7504ff6536d41fc13a111e2a6fe6c02ed8116361
Masta <muhammad.al.ghifari.fausta@mail.ugm.ac.id>
2023-09-21 16:39:05 +0700

    UI: admin menu | Class : Scheduling and Admin

---
+++ SmartDur/Scheduling.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartDur
+{
+    internal class Scheduling
+    {
+        //declaring attribute for scheduling
+        private int recommendedAmount;
+        private int fertilizerAmount;
+
+        private Crop crop;
+        private Fertilizer fertilizer;
+
+        public int getRecommendedAmount()
+        {
+            return recommendedAmount;
+        }
+        public int getFertilizerAmount()
+        {
+            return fertilizerAmount;
+        }
+        public Crop getCrop()
+        {
+            return crop;
+        }
+
+        public Fertilizer getFertilizer() { return fertilizer; }
+    }
+}
